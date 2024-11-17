35e3bf979ba1bc9cdfd84036bab49b4f845b3ba1
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-11-16 23:12:56 -0300

    feat: I added the option to login on the main page

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,5 @@
 import { useCart } from '../components/Context/CartContext';
+import { Link } from 'react-router-dom';
 
 import styles from '../css/Header.module.css'
 
@@ -9,7 +10,7 @@ function Header() {
     <header className={styles.header}>
       <div className={styles.container}>
         <h1 className={styles.title_total}>Total: ${total.toFixed(2)}</h1>
-        <p className={styles.login}>Fazer login</p>
+        <p className={styles.login}><Link to="/login" className={styles.link}>Fazer login</Link></p>
       </div>
     </header>
 
